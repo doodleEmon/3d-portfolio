5e05093ea066d4ad6e92af750d869e6e2e397770
Emon <150001421+doodleEmon@users.noreply.github.com>
2026-08-12 13:36:40 +0600

    The first icon not showing problem solved with react-intersection-observer package and by updating the Tech.jsx

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,16 +1,29 @@
 import React from "react";
+import { useInView } from "react-intersection-observer";
 
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
+import CanvasLoader from "./Loader";
+
+const LazyBall = ({ icon }) => {
+  const { ref, inView } = useInView({
+    triggerOnce: false,
+    rootMargin: "200px 0px", // mount slightly before fully in view
+  });
+
+  return (
+    <div ref={ref} className="w-28 h-28">
+      {inView ? <BallCanvas icon={icon} /> : <div className="w-28 h-28" />}
+    </div>
+  );
+};
 
 const Tech = () => {
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
       {technologies.map((technology) => (
-        <div className="w-28 h-28" key={technology.id}>
-          <BallCanvas icon={technology.icon} />
-        </div>
+        <LazyBall key={technology.id} icon={technology.icon} />
       ))}
     </div>
   );
